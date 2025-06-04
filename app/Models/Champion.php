d97d7af56e55cc3fb4a01b25d09bf33afec381fd
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-04 19:17:46 +0300

    fix models

--- app/Models/Champion.php
+++ app/Models/Champion.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Champion extends Model
+class Champion extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $fillable = [
         'name',
