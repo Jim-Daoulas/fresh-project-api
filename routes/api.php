ab7960339dcc891d46474898349b31e184b02b33
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-04 12:54:24 +0300

    add api.php

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'hello World']);
 });
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Test route works!']);
+});
+//Route::prefix('admin')->name('admin')->group(base_path('routes/admin.php'));
+Route::prefix('users')->name('users')->group(base_path('routes/users.php'));
+Route::prefix('champions')->name('champions')->group(base_path('routes/champions.php'));
