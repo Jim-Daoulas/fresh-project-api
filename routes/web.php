acf4e15779fb4718daf721b03bcf7730bd08b23e
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-04 18:07:29 +0300

    Filament authentication

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Προσθήκη για Filament authentication
+Route::post('/admin/login', function() {
+    // Αυτό θα το χειριστεί το Filament αυτόματα
+})->name('filament.admin.auth.login');
