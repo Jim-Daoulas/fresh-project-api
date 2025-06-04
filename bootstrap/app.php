41c61769a09884f5b11d0d2f5fe1c355449a0534
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-04 18:39:11 +0300

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,6 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         'admin/login',
         'admin/*'
     ]);
+    $middleware->trustProxies(at: '*');
 })
     ->withExceptions(function (Exceptions $exceptions) {
         //
