4998c6e809ca954730715fba679e3b3071f87c7f
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-07 19:15:15 +0300

    update user

--- routes/users.php
+++ routes/users.php
@@ -15,4 +15,13 @@ Route::prefix("user")
         Route::get("me", [UserController::class, 'me']);
         Route::get("tokens", [UserController::class, 'tokens']);
         Route::delete("revoke-all-tokens", [UserController::class, 'revokeAllTokens']);
+        
+        // Progression routes
+        Route::get("progress", [UserController::class, 'getProgress']);
+        Route::post("daily-bonus", [UserController::class, 'claimDailyBonus']);
+        Route::get("unlocks", [UserController::class, 'getAvailableUnlocks']);
+        Route::post("unlock/champion/{championId}", [UserController::class, 'unlockChampion']);
+        Route::post("unlock/skin/{skinId}", [UserController::class, 'unlockSkin']);
+        Route::post("track/champion-view", [UserController::class, 'trackChampionView']);
+        Route::post("track/comment", [UserController::class, 'trackComment']);
     });
