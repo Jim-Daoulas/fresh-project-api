3c96f39d53a06517adabaddd66da7d7004b94f85
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-09 20:13:07 +0300

    update middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,8 +10,16 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
+        // Debug log
+        \Log::info('CORS Middleware hit', [
+            'method' => $request->getMethod(),
+            'path' => $request->getPathInfo(),
+            'origin' => $request->header('Origin')
+        ]);
+
         // Handle preflight requests
         if ($request->getMethod() === "OPTIONS") {
+            \Log::info('Handling OPTIONS request');
             return response('', 200)
                 ->header('Access-Control-Allow-Origin', '*')
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
@@ -20,9 +28,12 @@ class CorsMiddleware
 
         $response = $next($request);
         
+        // Add CORS headers to all responses
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        
+        \Log::info('CORS headers added to response');
         
         return $response;
     }
