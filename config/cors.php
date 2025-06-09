f4fe5288f9bf29a1f4fd2909817cb7aa516d8599
Jim-Daoulas <daoulas.d.ace@gmail.com>
2025-06-09 21:03:13 +0300

    update cors

--- config/cors.php
+++ config/cors.php
@@ -4,6 +4,7 @@ return [
     'paths' => ['*'], // Όλα τα paths
     'allowed_methods' => ['*'],
     'allowed_origins' => [
+        'https://storied-salamander-061141.netlify.app',
         'http://localhost:5173',  // Vite dev server
         'http://localhost:3000',  
         'http://127.0.0.1:5173',
